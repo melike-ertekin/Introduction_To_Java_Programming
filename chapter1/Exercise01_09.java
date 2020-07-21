57a59c9d2f9b060291edf29046472b0ba05792d7
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 14:24:45 -0700

    Add Exercise01_09

---
+++ chapter1/Exercise01_09.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class Exercise01_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double w=4.5, h=7.9;
+		System.out.println("Perimeter: "+ 2 * (w+h));
+		System.out.println("Area: "+ w*h);
+		
+
+	}
+
+}
