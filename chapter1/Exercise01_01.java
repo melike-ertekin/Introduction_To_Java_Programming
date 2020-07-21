5839002e515718bee4688e477e520ccf33eb73ce
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 13:28:41 -0700

    Add exercise01_01

---
+++ chapter1/Exercise01_01.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Exercise01_01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Java!\nWelcome to Computer Science!\nProgramming is fun!");
+	}
+
+}
