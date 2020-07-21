810efa4e5cf45417b03d883512c6f752835a2f81
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 14:15:27 -0700

    Add Exercise01_07

---
+++ chapter1/Exercise01_07.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class Exercise01_07 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println( 4.0 * ( 1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0  + 
+				1.0 / 9.0 - 1.0 / 11.0 ) );
+		System.out.println( 4.0 * ( 1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0  + 
+				1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0) );
+
+	}
+
+}
