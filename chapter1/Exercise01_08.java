8e63c0b799b4ac26ec8667b811d07d1e36eaf9c4
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 14:20:00 -0700

    Add Exercise01_08

---
+++ chapter1/Exercise01_08.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class Exercise01_08 {
+
+	public static void main(String[] args) {
+		
+		double r = 5.5;
+		System.out.println("Perimeter: "+ 2 * Math.PI*r );
+		System.out.println("Area: "+ Math.PI * Math.pow(r, 2) );
+
+
+	}
+
+}
