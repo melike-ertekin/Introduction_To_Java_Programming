22c7fb00851c012bb86ab38817adbfbb3238c180
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 13:44:39 -0700

    Add Exercise01_04

---
+++ chapter1/Exercise01_04.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Exercise01_04 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("a     a^2     a^3");
+		
+		for(int i=1; i<5; i++) {
+			System.out.println( i +"   "+"   "+ i*i+ "       "+ i*i*i );
+			
+		}
+
+	}
+
+}
