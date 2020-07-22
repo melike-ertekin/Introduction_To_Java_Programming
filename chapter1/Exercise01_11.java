d902553dc36da467d334fdc8a342dcf7afd853b8
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 18:01:51 -0700

    Add Exercise01_11

---
+++ chapter1/Exercise01_11.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Exercise01_11 {
+		public static void main(String[] args) {
+			
+			int currentPop = 312032486;
+			int changeOfPop = (365*24*60*60/7) - (365*24*60*60/13) + (365*24*60*60/45);
+			
+			System.out.println("Year-1: "+ (currentPop + changeOfPop));
+			System.out.println("Year-2: "+ (currentPop + changeOfPop * 2));
+			System.out.println("Year-3: "+ (currentPop + changeOfPop * 3));
+			System.out.println("Year-4: "+ (currentPop + changeOfPop * 4));
+			System.out.println("Year-5: "+ (currentPop + changeOfPop * 5));
+			
+		}
+
+}
