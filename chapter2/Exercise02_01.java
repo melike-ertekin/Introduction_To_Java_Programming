29cfbe78010666526de7f2e6ac2479be4c610eb1
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-22 13:35:50 -0700

    Add Exercise02_01

---
+++ chapter2/Exercise02_01.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise02_01 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a degree in Celcius: ");
+		double celsius = input.nextDouble();
+		
+		double fahrenheit = celsius * 9 / 5.0  + 32;
+		
+		System.out.println(celsius + " Celcius is " + fahrenheit + " Fahrenheit.");
+
+	}
+
+}
