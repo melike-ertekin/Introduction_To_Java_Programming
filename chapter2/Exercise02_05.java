d2fea20667f4693e08fa11d49e328042de026253
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-22 14:07:38 -0700

    Add Exercise02_05

---
+++ chapter2/Exercise02_05.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise02_05 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the subtotal and a gratuity rate: ");
+		double subtotal = input.nextDouble();
+		double gratuity_rate = input.nextDouble();
+		
+		double gratuity = subtotal * gratuity_rate / 100;
+		double total = subtotal + gratuity;
+		
+		System.out.print("The gratuity is $" + gratuity + " and total is $" + total);
+
+		
+
+	}
+
+}
