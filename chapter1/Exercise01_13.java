87bcd5da23af933d3c0a228e1694e558c3eb2708
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-21 18:45:53 -0700

    Add Exercise01_13

---
+++ chapter1/Exercise01_13.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Exercise01_13 {
+
+	public static void main(String[] args) {
+		
+		double d = 3.4 * 0.55 - 2.1 * 50.2;
+		double dx = 44.5 * 0.55 - 5.9 * 50.2;
+		double dy = 3.4 * 5.9 - 2.1 * 44.5;
+		
+		System.out.println("x = " + dx/d);
+		System.out.println("y = " + dy/d);
+
+
+	}
+
+}
