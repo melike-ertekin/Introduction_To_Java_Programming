49086ce9ac14433c758490ec114b420b28010953
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-22 23:15:11 -0700

    Add Exercise02_08

---
+++ chapter2/Exercise02_08.java
@@ -0,0 +1,36 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise02_08 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		long totalMilliSec = System.currentTimeMillis();
+		long currentHour = (totalMilliSec / 3600000) % 24 ;
+		long currentMin = (totalMilliSec / 60000) % 60 ;
+		long currentSec = (totalMilliSec / 1000) % 60 ;
+		
+		System.out.println("Current Time in Greenwich: " + currentHour + ":" + currentMin + ":" + currentSec);
+		
+		System.out.print("Enter the time zone offset to GMT: ");
+		long timeOffSet = input.nextLong();
+		
+		if((currentHour + timeOffSet) < 0) {
+			currentHour = currentHour + timeOffSet + 24 ;
+		}
+		else if((currentHour + timeOffSet) > 24) {
+			currentHour = currentHour + timeOffSet - 24 ;
+		}
+		else {
+			currentHour = currentHour + timeOffSet;
+		}
+
+		System.out.println(currentHour + ":"+currentMin +":"+currentSec);
+		
+			
+	}
+
+}
