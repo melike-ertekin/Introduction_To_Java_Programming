58979ccafdddd28ad83ccad2fa5f7efbcb7823ab
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-22 23:44:31 -0700

    Add Exercise02_12

---
+++ chapter2/Exercise02_12.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise02_12 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter speed and acceleration: ");
+		float speed = input.nextFloat();
+		float acceleration = input.nextFloat();
+	
+		float result = speed * speed / (2 * acceleration);
+
+		System.out.println("The minimum runway length for this airplane is "+ result);
+
+	}
+
+}
