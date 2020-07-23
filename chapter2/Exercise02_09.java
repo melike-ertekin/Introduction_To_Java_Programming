b21503bfa29b76c9ce7b30ffad204edb0772e95d
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-22 23:20:30 -0700

    Add Exercise02_09

---
+++ chapter2/Exercise02_09.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise02_09 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter v0, v1, and t: ");
+		float v0 = input.nextFloat();
+		float v1 = input.nextFloat();
+		float t = input.nextFloat();
+		
+		float result = (v1 - v0) / t;
+		System.out.println("The average acceleration is "+ result);
+		
+
+	}
+
+}
