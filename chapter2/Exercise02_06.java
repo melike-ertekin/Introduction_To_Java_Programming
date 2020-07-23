50b0da59c61d8e532354ad61aef61fab57a2f788
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-22 20:17:26 -0700

    Add Exercise02_06

---
+++ chapter2/Exercise02_06.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise02_06 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number between 0 and 1000: ");
+		int number = input.nextInt();
+		int sum = 0;
+		
+		while(number != 0) {
+			sum = sum + number % 10; 
+			number = number / 10;
+		}
+		
+		System.out.print("The sum of the digits is " + sum);
+		
+	}
+
+}
