9ccc8b06e2e7cfdc28a094e170c14d419be6b729
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-26 00:23:34 -0700

    Add Exercise02_18

---
+++ chapter02/Exercise02_18.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+public class Exercise02_18 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a     b     pow(a,b)");
+		
+		for(int i = 1; i < 6; i++) {
+			System.out.println(i + "     " + (i+1) + "     " + (int) Math.pow(i, i+1));
+			
+		}
+
+	}
+
+}
