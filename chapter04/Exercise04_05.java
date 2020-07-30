07d32050b4f56adebc036a44e34f75899015eb64
melike-ertekin <melikeb.ertekin@gmail.com>
2020-07-30 11:59:08 -0700

    Add Exercise04_05

---
+++ chapter04/Exercise04_05.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class Exercise04_05 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the number of sides: ");
+		double n = input.nextDouble();
+
+		System.out.print("Enter the side: ");
+		double s = input.nextDouble();
+		
+		double area = (n * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / n));
+		System.out.print("The area of the polygon is " +area);
+		
+		
+	}
+
+}
